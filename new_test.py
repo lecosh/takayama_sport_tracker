c84fdb3f699c5fbf7ca5b51cd675b131ac7bf19a
krt494 <romankorotkov2004@yandx.ru>
2024-02-24 12:48:30 +0300

    d

--- new_test.py
+++ new_test.py
@@ -1,1 +1,1 @@
-print("ok")asdasdasdasd
+print("ok")
--- test.py
+++ test.py
@@ -1,4 +1,8 @@
-print("ROMA LOX")
-print("SAM TY LOX")
-print("BEBEBBE SAM LOX")
-print("pishov nahui")
+import telebot
+
+bot = telebot.TeleBot("6972868806:AAHUtfGbzbcryr7D6RZLOUD9CMg7FO8Csxk")
+@bot.message_handler(commands=['start'])
+def main(message):
+    bot.send_message(message.chat.id, 'Привет!')
+
+bot.polling(none_stop=True)
