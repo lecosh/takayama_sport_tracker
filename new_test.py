5d4fcafe3d8d7b19c6629fa323e122e1ac001d4a
krt494 <romankorotkov2004@yandx.ru>
2024-02-24 22:14:26 +0300

    edit_train

--- new_test.py
+++ new_test.py
@@ -23,6 +23,8 @@ class Choosing_excersize(StatesGroup):
 train_list = ["Первая тренировка", "Вторая тренировка", "Третья тренировка"]
 
 
+
+
 router = Router()
 bot = Bot(token=config.BOT_TOKEN, parse_mode=ParseMode.HTML)
 dp = Dispatcher(storage=MemoryStorage())
@@ -35,15 +37,31 @@ async def start_handler(msg:Message):
 
 @dp.message(Command("info"))
 async def info_handler(msg:Message):
-    await  msg.answer("Доступные комманды:\n/edit_train - Позволяет отредактировать выбранную тренировку.\
-                         \n/create_train - Создать тренировку.\
+    await  msg.answer("Доступные комманды:\n/create_train - Создать тренировку.\
                          \n/choose_standart_train - Выбрать тренировку из списка стандартных.\
                          \n/import_your_train - Импортировать свою тренировку. \
                          \n/edit_train - Редактировать тренировку.")
 
 
+
+@dp.message(F.text.in_(train_list), StateFilter(None))
+async def print_train(msg: types.Message, state: FSMContext):
+    await msg.answer(f"Происходит вывод выбранной тренировки", reply_markup=types.ReplyKeyboardRemove())
+    kb = [
+        [types.KeyboardButton(text="Удалить")],
+        [types.KeyboardButton(text="Изменить")]
+    ]
+    keyboard = types.ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True,
+                                         input_field_placeholder="Выберите действие над тренировкой:")
+    await msg.answer(f"Выберите действие над тренировкой:", reply_markup=keyboard)
+
+    #await msg.answer("происходит вывод выбранной тренировки")
+    # data = await state.get_data()
+    # print(data)
+
+
 @dp.message(Command("import_your_train"))
-async def import_handler(msg:Message):
+async def import_handler(msg: types.Message):
     kb = [
         types.KeyboardButton(text="CSV"),
         types.KeyboardButton(text="XLS")
@@ -53,19 +71,15 @@ async def import_handler(msg:Message):
 
 
 @dp.message(Command("edit_train"))
-async def import_edit(msg:Message):
+async def import_edit(msg: types.Message):
     kb = [
         [types.KeyboardButton(text=train_list[0])],
         [types.KeyboardButton(text=train_list[1])]
     ]
-    keyboard = types.ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
-    await msg.answer("Выберите тренировку.", reply_markup=keyboard)
+    keyboard = types.ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True, input_field_placeholder="Выберите тренировку:")
+    await msg.answer(f"Выберите тренировку:", reply_markup=keyboard)
 
 
-
-
-
-    
 
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)
