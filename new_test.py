96b4e335fafcffe35e0a4ffed11ec8133bfa7a94
krt494 <romankorotkov2004@yandx.ru>
2024-02-24 21:21:01 +0300

    edit_train

--- new_test.py
+++ new_test.py
@@ -1,6 +1,3 @@
-import telebot
-
-from telebot import types
 from aiogram import Router
 from aiogram import types, F, Router
 from aiogram.types import Message
@@ -10,7 +7,21 @@ import logging
 from aiogram import Bot, Dispatcher
 from aiogram.enums.parse_mode import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
+import regexp
+from aiogram.filters import Command, StateFilter
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
 import config
+
+
+
+class Choosing_excersize(StatesGroup):
+    type_exc = State()
+    number_exc = State()
+    exc_reps = State()
+
+train_list = ["Первая тренировка", "Вторая тренировка", "Третья тренировка"]
+
 
 router = Router()
 bot = Bot(token=config.BOT_TOKEN, parse_mode=ParseMode.HTML)
@@ -19,7 +30,7 @@ dp.include_router(router)
 
 @router.message(Command("start"))
 async def start_handler(msg:Message):
-    await msg.answer("Введите /info для просмотра справки доступных команд")
+    await msg.answer("Сегодня мы будем готовить мои любимые фритату-та-ту-та-ту. Для просмотра команд введите /info.")
 
 
 @dp.message(Command("info"))
@@ -27,18 +38,34 @@ async def info_handler(msg:Message):
     await  msg.answer("Доступные комманды:\n/edit_train - Позволяет отредактировать выбранную тренировку.\
                          \n/create_train - Создать тренировку.\
                          \n/choose_standart_train - Выбрать тренировку из списка стандартных.\
-                         \n/import_your_train - Импортировать свою тренировку")
+                         \n/import_your_train - Импортировать свою тренировку. \
+                         \n/edit_train - Редактировать тренировку.")
 
 
 @dp.message(Command("import_your_train"))
 async def import_handler(msg:Message):
     kb = [
-        [types.KeyboardButton(text="CSV")],
-        [types.KeyboardButton(text="XLS")]
+        types.KeyboardButton(text="CSV"),
+        types.KeyboardButton(text="XLS")
     ]
-    keyboard = types.ReplyKeyboardMarkup(keyboard=kb)
-    await msg.answer("Выберите формат: CSV или XLS.\
-                         \n\nНажмите на одну из кнопок ниже.", reply_markup=keyboard)
+    keyboard = types.ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
+    await msg.answer("Выберите формат: CSV или XLS. \n\nНажмите на одну из кнопок ниже.", reply_markup=keyboard)
+
+
+@dp.message(Command("edit_train"))
+async def import_edit(msg:Message):
+    kb = [
+        [types.KeyboardButton(text=train_list[0])],
+        [types.KeyboardButton(text=train_list[1])]
+    ]
+    keyboard = types.ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
+    await msg.answer("Выберите тренировку.", reply_markup=keyboard)
+
+
+
+
+
+    
 
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)
