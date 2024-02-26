8baf75f1fdd983e2811b489e8d6742a453e9a8fc
artemvolkofff <artemvolkoffpilot@yandex.ru>
2024-02-26 20:50:01 +0300

    123

--- bot_ready.py
+++ bot_ready.py
@@ -2,7 +2,6 @@ import telebot
 import config
 
 bot = telebot.TeleBot(config.API)
-
 @bot.message_handler(commands=['start'])
 def main(message):
     bot.send_message(message.chat.id, f'Здравствуйте, {message.from_user.first_name}!')
