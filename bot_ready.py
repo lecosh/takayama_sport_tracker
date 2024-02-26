297ea45ecb84bd0c30391f51dd32a873007129c6
artemvolkofff <artemvolkoffpilot@yandex.ru>
2024-02-26 20:38:18 +0300

    first version

--- bot_ready.py
+++ bot_ready.py
@@ -1,1 +1,10 @@
-print("hello")
+import telebot 
+import config
+
+bot = telebot.TeleBot(config.API)
+
+@bot.message_handler(commands=['start'])
+def main(message):
+    bot.send_message(message.chat.id, f'Здравствуйте, {message.from_user.first_name}!')
+
+bot.infinity_polling()
