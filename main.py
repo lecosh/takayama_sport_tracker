4c73fc8adbc297448266b026b31b7364061fcd16
lecosh <artem_vorobey_2001@mail.ru>
2024-04-23 23:41:24 +0300

    Style(main): Add comments

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import asyncio
 import logging
-
 from src.utils.constants import *
 from src.utils.util import *
 from src.create_train.create_exc import *
@@ -8,16 +7,21 @@ from src.create_train.choose_exist import *
 from src.create_train.create_train import *
 
 
+"""Starting bot. 
+Note: user will be added in database only if command "/start" will be triggered
+"""
 @dp.message(Command("start"))
 async def start_handler(msg: Message, state: FSMContext):
     await msg.answer("Привет!")
-    cursor.execute("INSERT OR REPLACE INTO Trains (id, train) VALUES (?, ?)", (msg.from_user.id, ""))
+    cursor.execute("INSERT OR REPLACE INTO Trains (id, train) VALUES (?, ?)", 
+                   (msg.from_user.id, ""))
     db.commit()
 
 
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
