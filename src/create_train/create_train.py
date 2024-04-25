0c55616da18b79c96a512f61f288f4b88faa092b
lecosh <artem_vorobey_2001@mail.ru>
2024-04-25 23:38:06 +0300

    Style(standart_train): Change location of comments
    Refs: PEP257

--- src/create_train/create_train.py
+++ src/create_train/create_train.py
@@ -7,12 +7,12 @@ from ..utils.constants import *
 from ..utils.util import *
 
 
-"""Handler for next excersize.
-This handler is triggered only if user choose one more excersize. Also state of bot
- is setting in "Choosing_excersize:add_next_exc".
-"""
 @dp.message(F.text.in_(add_next_excersize), StateFilter("Choosing_excersize:add_next_exc"))
 async def handle_next_exc_choose(msg: Message, state: FSMContext):
+    """Handler for next excersize.
+    This handler is triggered only if user choose one more excersize. Also state of bot
+    is setting in "Choosing_excersize:add_next_exc".
+    """
     if msg.text == 'Да':
         await state.set_state(Choosing_excersize.choose_creation_type)
         exc_buttons = [
@@ -31,12 +31,12 @@ async def handle_next_exc_choose(msg: Message, state: FSMContext):
         await state.set_state(None)
 
 
-"""Handler for "/create_train" command. 
-Here the user is asked to choose one of two exercise choices. Here bot is setting in 
-"Choosing_excersize:choose_creation_type" state. All states described in /src/utils/util.py
-"""
 @dp.message(Command("create_train"), StateFilter(None))
 async def amount_handler(msg: Message, state: FSMContext):
+    """Handler for "/create_train" command. 
+    Here the user is asked to choose one of two exercise choices. Here bot is setting in 
+    "Choosing_excersize:choose_creation_type" state. All states described in /src/utils/util.py
+    """
     await state.set_state(Choosing_excersize.choosen_train)
     await state.update_data(choosen_train="")
     await state.set_state(Choosing_excersize.choose_creation_type)
