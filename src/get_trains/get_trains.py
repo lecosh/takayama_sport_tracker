9d1a7ffa4f2b99c4efa30d20c3e890bf65910b03
lecosh <artem_vorobey_2001@mail.ru>
2024-04-29 18:06:34 +0300

    Feat(get_trains): Add get_trains command

---
+++ src/get_trains/get_trains.py
@@ -0,0 +1,24 @@
+from aiogram.filters import Command
+from ..utils.constants import *
+from ..utils.util import *
+
+
+@dp.message(Command("get_trains"))
+async def handle_get_trains(msg: types.Message):
+    cursor.execute("SELECT train FROM TRAINS WHERE ID = ?", (msg.from_user.id,))
+
+    rows = cursor.fetchall()
+    
+    result_list = [row[0] for row in rows]
+    if len(result_list) == 0:
+        await msg.answer('Тренировок нет')
+    else:
+        split_result = []
+        for train in result_list:
+            split_train = train.split('\n\n')
+            split_result.extend([part.strip() for part in split_train])
+        count = 1
+        for train in split_result:
+            await msg.answer(f'<b>Тренировка {count}</b>:', parse_mode=ParseMode.HTML)
+            await msg.answer(train)
+            count += 1
