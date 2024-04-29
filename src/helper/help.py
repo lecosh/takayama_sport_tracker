3dd89e87ef72c83d22fb044aa1b2f37bc0539d7f
lecosh <artem_vorobey_2001@mail.ru>
2024-04-30 01:15:00 +0300

    Fix(help): Edit command name

--- src/helper/help.py
+++ src/helper/help.py
@@ -6,7 +6,7 @@ from ..utils.constants import *
 @dp.message(Command("help"))
 async def info_handler(msg:Message):
     await msg.answer("Доступные команды:\n/create_train - Создать тренировку\
-                        \n/choose_standart_train - Выбрать тренировку из списка стандартных\
+                        \n/standart - Выбрать тренировку из списка стандартных\
                         \n/import_train - Импортировать свою тренировку\
                         \n/edit_train - Редактировать тренировку\
                         \n/get_trains - Список созданных тренировок\
